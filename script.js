a09bba10f122a99adfe0f67be1e65a89129d69a1
Brinkiii <155560550+Brinkiii@users.noreply.github.com>
2024-01-23 16:37:05 +0100

    first draft

---
+++ script.js
@@ -0,0 +1,19 @@
+let container = document.getElementById("container");
+let gridNumber = 16
+
+
+for (let i = 0; i < gridNumber; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    for (let j = 0; j < gridNumber; j++) {
+        let gridDiv = document.createElement("div");
+        gridDiv.classList.add("grid-cell"); 
+        row.appendChild(gridDiv);
+
+        gridDiv.addEventListener("mouseover", () => {
+            gridDiv.classList.add("filled")
+        })
+    }
+}
