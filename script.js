7c4cf4208d555118ce58f08f88eed63c9af387cb
Brinkiii <155560550+Brinkiii@users.noreply.github.com>
2024-01-24 14:20:34 +0100

    final version

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let container = document.getElementById("container");
 let gridSize = document.getElementById("gridSize");
 let clear = document.getElementById("clear")
+let rainbow = document.getElementById("rainbow");
+let black = document.getElementById("black");
+let red = document.getElementById("red");
+let green = document.getElementById("green");
+let blue = document.getElementById("blue");
 gridNumber = 16;
 
 function createGrid() {
@@ -17,10 +22,36 @@ function createGrid() {
             row.appendChild(gridDiv);
 
             gridDiv.addEventListener("mouseover", () => {
-                gridDiv.classList.add("filled");
+                gridDiv.style.backgroundColor = "#000000"
             });
             clear.addEventListener("click", () => {
-                gridDiv.classList.remove("filled")
+                gridDiv.style.backgroundColor = "#FFFFFF"
+            })
+            rainbow.addEventListener("click", () => {
+                gridDiv.addEventListener("mouseover", () => {
+                    let newRandomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+                    gridDiv.style.backgroundColor = newRandomColor;
+                });
+            })
+            black.addEventListener("click", () => {
+                gridDiv.addEventListener("mouseover", () => {
+                    gridDiv.style.backgroundColor = "#000000";
+                });
+            })
+            red.addEventListener("click", () => {
+                gridDiv.addEventListener("mouseover", () => {
+                    gridDiv.style.backgroundColor = "#FF0000";
+                });
+            })
+            green.addEventListener("click", () => {
+                gridDiv.addEventListener("mouseover", () => {
+                    gridDiv.style.backgroundColor = "#00FF00";
+                });
+            })
+            blue.addEventListener("click", () => {
+                gridDiv.addEventListener("mouseover", () => {
+                    gridDiv.style.backgroundColor = "#0000FF";
+                });
             })
         }
     }
@@ -39,3 +70,6 @@ gridSize.addEventListener("click", () => {
     // Create the new grid with the updated size
     createGrid();
 });
+
+let newRandomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+gridDiv.style.backgroundColor = newRandomColor;
